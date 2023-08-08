7a2bfed48d2177627a2891f6116740eff0e95a9d
woutervdbroeck <woutervdbroeck1@gmail.com>
2023-08-08 16:20:51 +0200

    run dev packages out of dockerfile

--- docker/docker.sh
+++ docker/docker.sh
@@ -5,4 +5,7 @@
 # --build flag builds the container if it wasn't already
 
 docker compose up -d --build;
+docker compose exec open3dml git clone https://github.com/woutervdbroeck/Open3D-ML;
+docker compose exec open3dml pip install -e ./Open3D-ML;
+docker compose exec open3dml pip install git+https://github.com/cavelab-ugent/pcl-segmentation;
 docker compose exec open3dml bash
