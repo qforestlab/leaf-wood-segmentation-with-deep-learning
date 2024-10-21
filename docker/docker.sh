210fea79df9a5b25cf8e4fa5ceeedadfa631bf46
woutervdbroeck <woutervdbroeck1@gmail.com>
2024-10-21 15:10:58 +0200

    clean up docker files

--- docker/docker.sh
+++ docker/docker.sh
@@ -7,5 +7,5 @@
 docker compose up -d --build;
 docker compose exec open3dml git clone https://github.com/woutervdbroeck/Open3D-ML;
 docker compose exec open3dml pip install -e ./Open3D-ML;
-docker compose exec open3dml pip install git+https://github.com/cavelab-ugent/pcl-segmentation;
+docker compose exec open3dml pip install git+https://github.com/qforestlab/leaf-wood-segmentation-with-deep-learning;
 docker compose exec open3dml bash
